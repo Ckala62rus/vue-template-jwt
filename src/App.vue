e42424f73f04c489425d3f30b054425eb76b8b47
Ckala <agr.akyla@mail.ru>
2023-03-30 20:16:19 +0300

    add metronic template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <router-link to="/">Домой</router-link>
-  <router-link to="/about">Обо мне</router-link>
+<!--  <router-link to="/">Домой</router-link>-->
+<!--  <router-link to="/about">Обо мне</router-link>-->
 
-  <router-link to="/login" v-if="!user">Войти</router-link>
+<!--  <router-link to="/login" v-if="!user">Войти</router-link>-->
 
-  <br>
-  <br>
-  <a href="#" @click="logout" v-if="user">Выход</a>
+<!--  <br>-->
+<!--  <br>-->
+<!--  <a href="#" @click="logout" v-if="user">Выход</a>-->
 
-  <br>
-  <br>
-  <router-link to="/me" v-if="user">Данные</router-link>
+<!--  <br>-->
+<!--  <br>-->
+<!--  <router-link to="/me" v-if="user">Данные</router-link>-->
+  <login-metronic v-if="!user" />
+  <main-template v-if="user"/>
 
-  <router-view></router-view>
 <!--  <img alt="Vue logo" src="./assets/logo.png">-->
 <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
@@ -22,11 +23,15 @@
 
 // import axios from "axios";
 import {mapGetters} from 'vuex'
+import LoginMetronic from "@/pages/auth/LoginMetronic";
+import MainTemplate from "@/pages/MainTemplate";
 
 export default {
   name: 'App',
 
   components: {
+    MainTemplate,
+    LoginMetronic
     // HelloWorld
   },
 
@@ -45,7 +50,6 @@ export default {
   },
 
   created() {
-
     let token = localStorage.getItem('token')
     let refreshToken = localStorage.getItem('refreshToken')
 
@@ -60,12 +64,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
 </style>
---
+++ src/pages/MainTemplate.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="d-flex flex-column flex-root">
+    <!--begin::Page-->
+    <div class="page d-flex flex-row flex-column-fluid">
+      <!--begin::Aside-->
+      <div id="kt_aside" class="aside aside-dark aside-hoverable" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_mobile_toggle">
+        <!--begin::Brand-->
+        <div class="aside-logo flex-column-auto" id="kt_aside_logo">
+          <!--begin::Logo-->
+          <a href="../../demo1/dist/index.html">
+            <img alt="Logo" src="template/media/logos/logo-1-dark.svg" class="h-25px logo" />
+          </a>
+          <!--end::Logo-->
+          <!--begin::Aside toggler-->
+          <div id="kt_aside_toggle" class="btn btn-icon w-auto px-0 btn-active-color-primary aside-toggle" data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="aside-minimize">
+            <!--begin::Svg Icon | path: icons/duotune/arrows/arr079.svg-->
+            <span class="svg-icon svg-icon-1 rotate-180">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path opacity="0.5" d="M14.2657 11.4343L18.45 7.25C18.8642 6.83579 18.8642 6.16421 18.45 5.75C18.0358 5.33579 17.3642 5.33579 16.95 5.75L11.4071 11.2929C11.0166 11.6834 11.0166 12.3166 11.4071 12.7071L16.95 18.25C17.3642 18.6642 18.0358 18.6642 18.45 18.25C18.8642 17.8358 18.8642 17.1642 18.45 16.75L14.2657 12.5657C13.9533 12.2533 13.9533 11.7467 14.2657 11.4343Z" fill="black" />
+									<path d="M8.2657 11.4343L12.45 7.25C12.8642 6.83579 12.8642 6.16421 12.45 5.75C12.0358 5.33579 11.3642 5.33579 10.95 5.75L5.40712 11.2929C5.01659 11.6834 5.01659 12.3166 5.40712 12.7071L10.95 18.25C11.3642 18.6642 12.0358 18.6642 12.45 18.25C12.8642 17.8358 12.8642 17.1642 12.45 16.75L8.2657 12.5657C7.95328 12.2533 7.95328 11.7467 8.2657 11.4343Z" fill="black" />
+								</svg>
+							</span>
+            <!--end::Svg Icon-->
+          </div>
+          <!--end::Aside toggler-->
+        </div>
+        <!--end::Brand-->
+        <!--begin::Aside menu-->
+        <aside-menu />
+        <!--end::Aside menu-->
+      </div>
+      <!--end::Aside-->
+      <!--begin::Wrapper-->
+      <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+        <!--begin::Header-->
+        <div id="kt_header" style="" class="header align-items-stretch">
+          <!--begin::Container-->
+          <div class="container-fluid d-flex align-items-stretch justify-content-between">
+            <!--begin::Aside mobile toggle-->
+            <div class="d-flex align-items-center d-lg-none ms-n3 me-1" title="Show aside menu">
+              <div class="btn btn-icon btn-active-light-primary w-30px h-30px w-md-40px h-md-40px" id="kt_aside_mobile_toggle">
+                <!--begin::Svg Icon | path: icons/duotune/abstract/abs015.svg-->
+                <span class="svg-icon svg-icon-2x mt-1">
+										<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+											<path d="M21 7H3C2.4 7 2 6.6 2 6V4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6C22 6.6 21.6 7 21 7Z" fill="black" />
+											<path opacity="0.3" d="M21 14H3C2.4 14 2 13.6 2 13V11C2 10.4 2.4 10 3 10H21C21.6 10 22 10.4 22 11V13C22 13.6 21.6 14 21 14ZM22 20V18C22 17.4 21.6 17 21 17H3C2.4 17 2 17.4 2 18V20C2 20.6 2.4 21 3 21H21C21.6 21 22 20.6 22 20Z" fill="black" />
+										</svg>
+									</span>
+                <!--end::Svg Icon-->
+              </div>
+            </div>
+            <!--end::Aside mobile toggle-->
+            <!--begin::Wrapper-->
+            <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
+              <!--begin::Navbar-->
+              <div class="d-flex align-items-stretch" id="kt_header_nav"></div>
+              <!--end::Navbar-->
+              <!--begin::Topbar-->
+              <div class="d-flex align-items-stretch flex-shrink-0">
+                <!--begin::Toolbar wrapper-->
+                <div class="d-flex align-items-stretch flex-shrink-0">
+                  <!--begin::User-->
+                  <user-menu />
+                  <!--end::User -->
+                  <!--begin::Heaeder menu toggle-->
+                  <div class="d-flex align-items-center d-lg-none ms-2 me-n3" title="Show header menu">
+                    <div class="btn btn-icon btn-active-light-primary w-30px h-30px w-md-40px h-md-40px" id="kt_header_menu_mobile_toggle">
+                      <!--begin::Svg Icon | path: icons/duotune/text/txt001.svg-->
+                      <span class="svg-icon svg-icon-1">
+													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+														<path d="M13 11H3C2.4 11 2 10.6 2 10V9C2 8.4 2.4 8 3 8H13C13.6 8 14 8.4 14 9V10C14 10.6 13.6 11 13 11ZM22 5V4C22 3.4 21.6 3 21 3H3C2.4 3 2 3.4 2 4V5C2 5.6 2.4 6 3 6H21C21.6 6 22 5.6 22 5Z" fill="black" />
+														<path opacity="0.3" d="M21 16H3C2.4 16 2 15.6 2 15V14C2 13.4 2.4 13 3 13H21C21.6 13 22 13.4 22 14V15C22 15.6 21.6 16 21 16ZM14 20V19C14 18.4 13.6 18 13 18H3C2.4 18 2 18.4 2 19V20C2 20.6 2.4 21 3 21H13C13.6 21 14 20.6 14 20Z" fill="black" />
+													</svg>
+												</span>
+                      <!--end::Svg Icon-->
+                    </div>
+                  </div>
+                  <!--end::Heaeder menu toggle-->
+                </div>
+                <!--end::Toolbar wrapper-->
+              </div>
+              <!--end::Topbar-->
+            </div>
+            <!--end::Wrapper-->
+          </div>
+          <!--end::Container-->
+        </div>
+        <!--end::Header-->
+        <!--begin::Content-->
+        <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+          <!--begin::Post-->
+          <div class="post d-flex flex-column-fluid" id="kt_post">
+            <!--begin::Container-->
+            <div id="kt_content_container" class="container-xxl">
+              <!--begin::Home card-->
+              <div class="card">
+                <!--begin::Body-->
+                <div class="card-body p-lg-20">
+                  <!--begin::Section-->
+                  <div class="mb-17">
+                    <!--begin::Title-->
+                    <h3 class="text-black mb-7">Latest Articles, News &amp; Updates</h3>
+                    <!--end::Title-->
+                    <!--begin::Separator-->
+                    <div class="separator separator-dashed mb-9"></div>
+                    <!--end::Separator-->
+                    <!--begin::Row-->
+                    <div class="row">
+                      <router-view></router-view>
+                    </div>
+                    <!--begin::Row-->
+                  </div>
+                  <!--end::Section-->
+                </div>
+                <!--end::Body-->
+              </div>
+              <!--end::Home card-->
+            </div>
+            <!--end::Container-->
+          </div>
+          <!--end::Post-->
+
+        </div>
+        <!--end::Content-->
+        <!--begin::Footer-->
+        <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
+          <!--begin::Container-->
+          <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+            <!--begin::Copyright-->
+            <div class="text-dark order-2 order-md-1">
+              <span class="text-muted fw-bold me-1">2021©</span>
+              <a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Keenthemes</a>
+            </div>
+            <!--end::Copyright-->
+            <!--begin::Menu-->
+            <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
+              <li class="menu-item">
+                <a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
+              </li>
+              <li class="menu-item">
+                <a href="https://keenthemes.com/support" target="_blank" class="menu-link px-2">Support</a>
+              </li>
+              <li class="menu-item">
+                <a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
+              </li>
+            </ul>
+            <!--end::Menu-->
+          </div>
+          <!--end::Container-->
+        </div>
+        <!--end::Footer-->
+      </div>
+      <!--end::Wrapper-->
+    </div>
+    <!--end::Page-->
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import UserMenu from "@/pages/template/UserMenu";
+import AsideMenu from "@/pages/template/AsideMenu";
+
+export default {
+  name: "MainTemplate",
+  components: {AsideMenu, UserMenu},
+  methods:{
+    logout(){
+      localStorage.removeItem('token')
+      localStorage.removeItem('refreshToken')
+      this.$store.dispatch('user', null)
+      // window.location.href = '/login'
+      this.$router.push('/login')
+    }
+  },
+
+  computed: {
+    ...mapGetters(['user'])
+  },
+
+  created() {
+    document.body.className = '';
+    document.body.classList.add('header-fixed', 'header-tablet-and-mobile-fixed', 'toolbar-enabled', 'toolbar-fixed', 'aside-enabled', 'aside-fixed')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
